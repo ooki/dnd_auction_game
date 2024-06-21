1845ab7344349ba30a30299272c4615c8ca3ee25
ooki <thetimon@gmail.com>
2024-06-21 15:12:32 +0200

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 
-__version__ = "0.1.3"
+__version__ = "0.1.5"
 
 setup(
     name="dnd_auction_game",
